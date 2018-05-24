4d9c9b0cb9a3a45d1bf38d712137e25e974a2471
Dejana <dejana.brankovic0605@gmail.com>
2018-05-24 20:25:16 +0200

    Added new methods.

--- src/Car.java
+++ src/Car.java
@@ -1,19 +1,28 @@
+
 public class Car {
-    
+
     int maxSpeed = 120;
     int minSpeed = 10;
     double weight = 1000;
     boolean isTheCarOn = false;
     String license = "ABC-123";
-    char condition = 'A'; 
+    char condition = 'A';
     double currentPrice = 3000;
     int mileagePassed = 50000;
-    
+
+    String model;
+    int maxFuel;
+    int currentFuel;
+    int consumption;
+
     public Car() {
-        
+        this.model = "default";
+        this.currentFuel = 0;
+        this.maxFuel = 100;
     }
+
     public Car(int maxSpeed, int minSpeed, double weight, boolean isTheCarOn, String license, char condition, double currentPrice, int mileagePassed) {
-        
+
         this.maxSpeed = maxSpeed;
         this.minSpeed = minSpeed;
         this.weight = weight;
@@ -22,25 +31,76 @@ public class Car {
         this.condition = condition;
         this.currentPrice = currentPrice;
         this.mileagePassed = mileagePassed;
+
     }
-    
-    public void changeMileage(int newMileage){
-       int diff; 
-       int multiplyChange;
-       
-        if(newMileage > mileagePassed){
- 
+
+    public void changeMileage(int newMileage) {
+        double diff;
+        double multiplyChange;
+
+        if (newMileage > mileagePassed) {
+
             diff = newMileage - mileagePassed;
             multiplyChange = diff / 10000;
-            this.currentPrice = currentPrice - multiplyChange * 300;
+            this.currentPrice = currentPrice - Math.round(multiplyChange) * 300;
             this.mileagePassed = newMileage;
         }
-        if(newMileage < mileagePassed){
-            
+        if (newMileage < mileagePassed) {
+
             diff = mileagePassed - newMileage;
             multiplyChange = diff / 10000;
-            this.currentPrice = currentPrice + multiplyChange * 300;
+            this.currentPrice = currentPrice + Math.round(multiplyChange) * 300;
             this.mileagePassed = newMileage;
         }
+    }
+
+    public void printMainAttributes() {
+
+        System.out.println("Model automobila je: " + this.model);
+        System.out.println("Maksimalan kapaciitet rezervoara je: " + this.maxFuel);
+        System.out.println("Trenutno je u rezervoaru: " + this.currentFuel);
+        System.out.println("Predjena kilometraza je: " + this.mileagePassed);
+        System.out.println("Potrosnja: " + this.consumption);
+        System.out.println(" ");
+
+    }
+
+    public void printAttributes() {
+
+        System.out.println("Maksimalna brzina: " + this.maxSpeed);
+        System.out.println("Minimalna brzina: " + this.minSpeed);
+        System.out.println("Tezina automobila je: " + this.weight);
+        System.out.println("Registracija je: " + this.license);
+        System.out.println("Cena automobila je: " + this.currentPrice);
+        System.out.println("Predjena kilometraza je: " + this.mileagePassed);
+        System.out.println(" ");
+
+    }
+
+    public void changeModel(String customModel) {
+        this.model = customModel;
+    }
+
+    public void changeMaxFuel(int customMaxFuel) {
+        this.maxFuel = customMaxFuel;
+    }
+
+    public void fuelUp() {
+        this.currentFuel = this.maxFuel;
+    }
+
+    public void changeConsumptio(int customConsumption) {
+        this.consumption = customConsumption;
+    }
+
+    public void travel(int distance) {
+
+        if (this.currentFuel > distance * this.consumption) {
+            this.mileagePassed = this.mileagePassed + distance;
+            this.currentFuel = this.currentFuel - distance * this.consumption;
+        } else {
+            System.out.println("There is no enough fuel.");
+        }
+
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,19 @@ public class Main {
     public static void main(String[] args) {
 
         Car myCar = new Car();
-
-        System.out.println("Maksimalna brzina je: " + myCar.maxSpeed + " km/h.");
-        System.out.println(myCar.condition);
-        System.out.println(myCar.license);
-        System.out.println(myCar.weight);
-        System.out.println(myCar.minSpeed);
         
+        myCar.printMainAttributes();
+
         Car companyCar = new Car(150, 30, 2000, false, "AB-554", 'B', 3500, 75000);
+        companyCar.changeModel("Fiat");
+        companyCar.changeMaxFuel(150);
+        companyCar.fuelUp();
+        companyCar.changeConsumptio(10);
         System.out.println("Maksimalna brzina je: " + companyCar.maxSpeed + " km/h.");
+        companyCar.travel(10);
+        companyCar.printMainAttributes();
+        
+        companyCar.printMainAttributes();
         
         companyCar.changeMileage(85000);
         System.out.println(companyCar.mileagePassed);
@@ -21,6 +25,18 @@ public class Main {
         myCar.changeMileage(35000);
         System.out.println(myCar.mileagePassed);
         System.out.println(myCar.currentPrice);
+        
+        Car jenniesCar = new Car();
+        
+        jenniesCar.changeMileage(10000);
+        System.out.println("Cena je: " + jenniesCar.currentPrice);
+        
+        Car audi = new Car();
+        audi.changeModel("Audi");
+        audi.fuelUp();
+        audi.changeConsumptio(5);
+        audi.travel(15);
+        audi.printMainAttributes();
     }
 
 }
